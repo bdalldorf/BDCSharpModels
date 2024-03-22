6c19306d73a7abeb280811f6372cde70f1193384
Bret Dalldorf <bret.dalldorf@gmail.com>
2024-03-22 02:25:45 -0700

    feat(app): Added Video Info Model

---
+++ Video/VideoInfoModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDCSharpModels.Video
+{
+    public class VideoInfoModel
+    {
+        public string UniqueID { get; set; } = string.Empty;
+        public string WebPageURL { get; set; } = string.Empty;
+        public string WebPageFilePath { get; set; } = string.Empty;
+        public string WebPageTemplatePath { get; set; } = string.Empty;
+        public string VideoTitle { get; set; } = string.Empty;
+        public string VideoDescription { get; set; } = string.Empty;
+        public string VideoFileURL { get; set; } = string.Empty;
+        public string VideoFilePath { get; set; } = string.Empty;
+        public byte[] VideoBytes { get; set; } = new byte[0];
+        public string PreviewImageURL {  get; set; } = string.Empty;
+        public string PreviewImagePath { get; set; } = string.Empty;
+        public byte[] PreviewImageBytes { get; set; } = new byte[0];
+    }
+}
